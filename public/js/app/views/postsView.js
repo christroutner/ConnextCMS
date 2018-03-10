b2e4b5cef46ae0f634c80dc90cc0d8789f56d5cb
Chris Troutner <chris.troutner@gmail.com>
2018-03-10 07:20:02 -0800

    Fixing bug in post edit links

--- public/js/app/views/postsView.js
+++ public/js/app/views/postsView.js
@@ -10,8 +10,8 @@ define([
 	var PostsView = Backbone.View.extend({
 
 		tagName:  'div',
-    
-    el: '#postsView',  
+
+    el: '#postsView',
 
 		template: _.template(PostsTemplate),
 
@@ -24,21 +24,21 @@ define([
 
 		},
 
-    render: function () {      
+    render: function () {
       //debugger;
-      
+
       this.$el.html(this.template);
 
       this.populateTable();
-      
+
       this.loadHelp();
-      
+
 			return this;
 		},
-    
+
     populateTable: function() {
       //debugger;
-      
+
       //Get a the sortIndex based on the date entry of the posts.
       var sortIndex = this.getDateIndex();
       //Error Handling
@@ -47,22 +47,22 @@ define([
           sortIndex.push(i);
         }
       }
-      
+
       //Loop through each model in the collection.
       //for( var i = 0; i < global.postsCollection.length; i++ ) {
       //for( var i = global.postsCollection.length-1; i > -1; i-- ) { //Show newest first
       for( var i=0; i < sortIndex.length; i++) {
-      
+
         try {
           //debugger;
 
           var model = global.postsCollection.models[sortIndex[i]];
-          
+
           //Handle corner case of new install with empty DB
           if( (global.postsCollection.models.length == 1) && (model.id == "") ) {
             return;
           }
-          
+
           //Clone the example row provided in the template.
           var tempRow = global.postsView.$el.find('#postRow').clone();
 
@@ -72,8 +72,8 @@ define([
           //Populate the new row with data from the model.
           var postTitle = model.get('title');
           tempRow.find('th').html('<a href="#/">'+postTitle+'</a>');
-          tempRow.find('th').find('a').attr('onclick', 'global.postsView.editPost('+i+')');
-        
+          tempRow.find('th').find('a').attr('onclick', 'global.postsView.editPost('+sortIndex[i]+')');
+
           //Cross-reference the author ID with a name from the userCollection. Display the authors name.
           var authorId = model.get('author');
           for(var j=0; j < global.userCollection.length; j++) {
@@ -84,12 +84,12 @@ define([
               break;
             }
           }
-          
+
           //Find and display the category name for this post.
           for( var j=0; j < global.postCategoryCollection.models.length; j++ ) {
             //To-Do: handle posts that are assigned no sections.
             var postCategoryGUID = model.get('categories')[0];
-            
+
             //Match up the GUIDs and display the name of the matching category.
             if( global.postCategoryCollection.models[j].id == postCategoryGUID ) {
               tempRow.find('.postCategories').text(global.postCategoryCollection.models[j].get('name'));
@@ -107,67 +107,67 @@ define([
 
           //Append the new row to the DOM.
           global.postsView.$el.find('#postsTable').append(tempRow);
-          
+
         } catch(err) {
           console.error('Error encountered in postsView.populateTable(). Error message:');
           console.error(err.message);
-          
+
           log.push('Error encountered in postsView.populateTable(). Error message:')
           log.push(err.message)
           sendLog();
         }
-        
+
       }
-      
+
     },
-    
+
     editPost: function(model_index) {
       //debugger;
-      
+
       $('#postsView').hide();
       $('#postsAddNewView').show();
-      
+
       $('#app-location').text('Posts : Edit Post');
-      
+
       //Load the currently selected model into the TinyMCE state variable so that once
       //the TinyMCE editor has been loaded, it knows which model to load.
       global.tinymce.currentModelIndex = model_index;
-      
+
       //Render the Add New pages View view.
       global.postsAddNewView.render();
-      
+
     },
-    
+
     loadHelp: function() {
       //debugger;
       $('#control-sidebar-home-tab').empty();
-      
+
       var wrapperHeight = $('.content-wrapper').height();
-      
+
       //console.log($('.wrapper').height());
       $('#control-sidebar-home-tab').css('overflow-y', 'scroll');
       $('#control-sidebar-home-tab').load('/documentation/core/posts.html', function() {
         $('#control-sidebar-home-tab').height(wrapperHeight);
       });
     },
-    
+
     // This function generates a sorted index of entries in the global.postsCollection
     // based on published date.
     getDateIndex: function() {
-      
+
       var sortedIndex = []; //Returned value.
-      
+
       //Error handling
       if(global.postsCollection.length == 0)
         return sortedIndex;
-      
+
       //Create an array of objects containing the timestamp and the index
       var sortedItems = [];
       for(var i=0; i < global.postsCollection.length; i++) {
         var thisDate = new Date(global.postsCollection.models[i].get('publishedDate'));
         sortedItems.push({dateStamp: thisDate.getTime(), index: i});
       }
-      
+
       //Sort the array of objects based on the timestamp.
       var sortFunc = function(a,b) {
         if(a.dateStamp < b.dateStamp)
@@ -177,16 +177,16 @@ define([
         return 0;
       }
       sortedItems.sort(sortFunc);
-      
-      //Pick out the new index based on the timestamp sorting.      
+
+      //Pick out the new index based on the timestamp sorting.
       for(var i=0; i < sortedItems.length; i++) {
         sortedIndex.push(sortedItems[i].index);
       }
-      
+
       //Return the sorted index.
       return sortedIndex;
     }
-    
+
 
 	});
 
